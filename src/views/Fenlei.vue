91feb3b3ef6f007dac61587b7ba7d9c528ec0405
echo-huxin <78082983+echo-huxin@users.noreply.github.com>
2021-05-12 22:51:56 +0800

    Revert "刘敖第一次修改"

--- src/views/Fenlei.vue
+++ src/views/Fenlei.vue
@@ -41,8 +41,8 @@
                 :key="index2"
               >
                 <router-link :to="`/hotsale/${j.titls}`">
-                  <img :src="j.imgs" style="width: 100%;" alt />
-                  <div class="f3_title">{{ j.titls }}</div>
+                  <img :src="j.imgs" style="width: 100%" alt />
+                  <div>{{ j.titls }}</div>
                 </router-link>
               </div>
             </div>
@@ -63,7 +63,7 @@
           slot="icon"
         />
         <img v-else src="../assets/首页1.png" alt="" slot="icon" />
-        <span v-if="selected == 'home'" style="color: red;">首页</span>
+        <span v-if="selected == 'home'" style="color: red">首页</span>
         <span v-else>首页</span>
       </mt-tab-item>
       <mt-tab-item id="fenlei">
@@ -74,7 +74,7 @@
           slot="icon"
         />
         <img v-else src="../assets/分类1.png" alt="" slot="icon" />
-        <span v-if="selected == 'feilei'" style="color: red;">分类</span>
+        <span v-if="selected == 'feilei'" style="color: red">分类</span>
         <span v-else>分类</span>
       </mt-tab-item>
       <mt-tab-item id="cart">
@@ -85,7 +85,7 @@
           slot="icon"
         />
         <img v-else src="../assets/购物车1.png" alt="" slot="icon" />
-        <span v-if="selected == 'cart'" style="color: red;">购物车</span>
+        <span v-if="selected == 'cart'" style="color: red">购物车</span>
         <span v-else>购物车</span>
       </mt-tab-item>
       <mt-tab-item id="me">
@@ -96,7 +96,7 @@
           slot="icon"
         />
         <img v-else src="../assets/我1.png" alt="" slot="icon" />
-        <span v-if="selected == 'me'" style="color: red;">我</span>
+        <span v-if="selected == 'me'" style="color: red">我</span>
         <span v-else>我</span>
       </mt-tab-item>
     </mt-tabbar>
@@ -107,82 +107,82 @@
 export default {
   data() {
     return {
-      selected: 'fenlei',
-      show: '1',
+      selected: "fenlei",
+      show: "1",
       fenleiList: {
         category1: {}, //表一
         category2: [], //表二
         text_obj: [], //表三
       },
-    }
+    };
   },
   mounted() {
     //请求商品类别数据
-    this.axios.get('sw/prc').then((result) => {
+    this.axios.get("sw/prc").then((result) => {
       //console.log(result);
       //用data中的category去接收axios中的category
       //一级表  sw_category1
-      this.fenleiList.category1 = result.data.category1
+      this.fenleiList.category1 = result.data.category1;
 
       //二级表
-      this.fenleiList.category2 = result.data.category2
+      this.fenleiList.category2 = result.data.category2;
       //console.log(this.fenleiList.category2);
 
       //三级表
       for (let i = 0; i < result.data.category2.length; i++) {
         // 将不标准的格式json对象用eval方法进行处理转化为数组对象
-        let obj = eval(result.data.category2[i].text_obj)
+        let obj = eval(result.data.category2[i].text_obj);
         // 将每次取出,转化后的数据push到text_obj
 
         obj.forEach((item, i) => {
           if (item.imgs) {
-            obj[i].imgs = require(`../assets/image/${item.imgs}`)
+            obj[i].imgs = require(`../assets/image/${item.imgs}`);
           }
-        })
-        this.fenleiList.text_obj.push(obj)
+        });
+        this.fenleiList.text_obj.push(obj);
       }
-    })
+    });
   },
   methods: {
     open(show) {
-      this.show = show
-      this.fenleiList.text_obj = []
+      this.show = show;
+      this.fenleiList.text_obj = [];
       // console.log(show);
       this.axios.get(`/sw/prc/towTable?firstId=${show}`).then((result) => {
-        this.fenleiList.category2 = result.data
+        this.fenleiList.category2 = result.data;
         //三级表
         for (let i = 0; i < this.fenleiList.category2.length; i++) {
           // 将不标准的格式json对象用eval方法进行处理转化为数组对象
-          let obj = eval(this.fenleiList.category2[i].text_obj)
+          let obj = eval(this.fenleiList.category2[i].text_obj);
 
           obj.forEach((item, i) => {
             if (item.imgs) {
-              obj[i].imgs = require(`../assets/image/${item.imgs}`)
+              obj[i].imgs = require(`../assets/image/${item.imgs}`);
             }
-          })
+          });
 
           // 将每次取出,转化后的数据push到text_obj
-          this.fenleiList.text_obj.push(obj)
+          this.fenleiList.text_obj.push(obj);
         }
         //console.log(this.fenleiList.text_obj);
-      })
+      });
     },
   },
 
   watch: {
     selected(newval, oldav1) {
-      if (newval == 'fenlei') {
-        this.$router.push('fenlei')
-      } else if (newval == 'cart') {
-        this.$router.push('cart')
-      } else if (newval == 'me') {
-        this.$router.push('me')
-      } else if (newval == 'home') {
-        this.$router.push('/')
+      if (newval == "fenlei") {
+        this.$router.push("fenlei");
+      } else if (newval == "cart") {
+        this.$router.push("cart");
+      } else if (newval == "me") {
+        this.$router.push("me");
+      } else if (newval == "home") {
+        this.$router.push("/");
       }
     },
   },
-}
+};
 </script>
 <style scoped>
 .feilei .tabbar {
@@ -200,12 +200,9 @@ export default {
 .feilei .tab .tablinks {
   width: 100%;
   /* display: block; */
-  font-size: 0.7rem; /*20*/
+  font-size: 1rem; /*20*/
   height: 2.5rem; /*50*/
   border: none;
-  color: rgb(116, 120, 122);
-  font-weight: 300;
-  background-color: white;
 }
 /* .tabbar .tab .tablinks:hover {
   background: #ccc;
@@ -222,12 +219,9 @@ export default {
 }
 .feilei .cards .card .cardTi {
   width: 100%;
+  font-size: 0.75rem;
+  font-weight: bold;
   text-align: center;
-  margin-bottom: 0.5rem;
-  color: rgb(54, 52, 51);
-  font-size: 0.6rem;
-  font-weight: 800;
-  line-height: 1rem;
 }
 .feilei .cards .card .cdBiao {
   /* border: 1px solid #f00; */
@@ -240,14 +234,7 @@ export default {
   text-align: center;
   margin: 3px;
 }
-.feilei .cardChild .f3_title {
-  color: rgb(153, 153, 153);
-  font-size: 0.6rem;
-  line-height: 0.8rem;
-  margin-top: 0.2rem;
-}
-.feilei .tab .active {
-  background: rgb(230, 222, 222);
-  color: rgb(198, 47, 46);
+.feilei .active {
+  background: #ccc;
 }
 </style>
--- src/views/Hotsale.vue
+++ src/views/Hotsale.vue
@@ -1,19 +1,6 @@
 <template>
   <div class="hots">
-    <!-- 顶部搜索框 -->
-    <div class="tops">
-      <div>
-        <van-icon
-          @click="fanhui"
-          name="arrow-left"
-          color="#f00"
-          size="1.5rem"
-        />
-      </div>
-      <div>
-        <van-search placeholder="请输入搜索关键词" />
-      </div>
-    </div>
+    <mt-search class="aa"></mt-search>
     <div class="hots-wb">
       <div class="hots-zh" @click="xial">综合</div>
       <div class="host-xl">销量</div>
@@ -35,14 +22,12 @@
     <div class="hots-x">
       <div class="hots-xu" v-for="item of list" :key="item.pid">
         <router-link :to="`/article/${item.pid}`">
-          <div class="showImage">
-            <img :src="item.image" alt="" width="100" height="100" />
-          </div>
-          <div class="title">{{ item.title }}</div>
-          <div class="author">
+          <div><img :src="item.image" alt="" width="100" height="100" /></div>
+          <div>{{ item.title }}</div>
+          <div>
             <p>{{ item.author }}</p>
           </div>
-          <div class="price">
+          <div>
             <span>¥{{ item.price }}</span>
           </div>
         </router-link>
@@ -52,19 +37,19 @@
 </template>
 <script>
 export default {
-  props: ['className'],
+  props: ["className"],
   data() {
     return {
       isshow: true,
       productsList: [],
       offset: 0,
       list: [],
-    }
+    };
   },
 
   mounted() {
     this.axios
-      .get('/sw/productList/', {
+      .get("/sw/productList/", {
         params: {
           cname: this.className,
           offset: this.offset,
@@ -72,35 +57,32 @@ export default {
       })
       .then((res) => {
         // console.log(res.data);
-        let i = res.data.data
+        let i = res.data.data;
         i.forEach((item) => {
           try {
             if (item.image[0]) {
-              item.image = JSON.parse(item.image)[0]
-              item.image = require('../assets/image/' + item.image)
+              item.image = JSON.parse(item.image)[0];
+              item.image = require("../assets/image/" + item.image);
             }
           } catch {
-            console.log(item.image)
+            console.log(item.image);
           }
-        })
-        console.log(i)
-        this.list = i
-      })
+        });
+        console.log(i);
+        this.list = i;
+      });
   },
 
   methods: {
-    fanhui() {
-      this.$router.push('/fenlei')
-    },
     xial() {
       if (this.isshow == true) {
-        this.isshow = false
+        this.isshow = false;
       } else {
-        this.isshow = true
+        this.isshow = true;
       }
     },
   },
-}
+};
 </script>
 
 <style scoped>
@@ -159,49 +141,19 @@ export default {
 }
 .hots-xu {
   width: 50%;
-  padding-bottom: 1.5rem;
 }
-.hots-xu .showImage img {
-  width: 100%;
-  height: 100%;
+.hots-xu > div > img {
+  height: 10rem;
 }
-.hots-xu .title {
-  -webkit-line-clamp: 2;
-  color: rgb(54, 52, 51);
+.hots-xu > div {
   font-size: 0.7rem;
-  line-height: 0.9rem;
-  margin-bottom: 0.2rem;
-  min-height: 1.75rem;
-  margin-top: 0.6rem;
+  margin-bottom: 0.3rem;
 }
-.hots-xu .author {
-  color: rgb(153, 153, 153);
-  font-size: 0.6rem;
-  margin-bottom: 0.4rem;
+.hots-xu > div > p {
+  font-size: 0.2rem;
 }
-.hots-xu .price {
-  color: rgb(198, 47, 46);
+.hots-xu > div > span {
   font-size: 0.8rem;
-  line-height: 1.1rem;
-}
-.hots-xu .showImage {
-  width: 8.6rem;
-  height: 8.6rem;
-}
-.hots .tops {
-  width: 100%;
-  height: 2.5rem;
-  display: flex;
-}
-.hots .tops div:first-child {
-  width: 10%;
-  height: 100%;
-}
-.hots .tops div:last-child {
-  width: 90%;
-  height: 100%;
-}
-.hots .van-icon {
-  margin-top: 0.5rem;
+  color: red;
 }
 </style>
